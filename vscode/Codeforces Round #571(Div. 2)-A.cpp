087e4db7d180aadda97203387eddc9aaeb360bf1
HXYerror <zx841985525@163.com>
2019-07-13 11:51:53 +0800

    7.12

---
+++ vscode/Codeforces Round #571(Div. 2)-A.cpp
@@ -0,0 +1,41 @@
+//#include<bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <vector>
+#include <queue>
+#include <stack>
+#define LL long long
+using namespace std;
+const int MAX = 200010;
+const int MAX_1 = 1000100;
+const int INF = 0x3f3f3f3f;//1061109567,1e9,int-MAX2147483647;
+const double EPS = 0.0000001;
+const int MOD = 998244353;
+int T,N,M;
+/*-------------------------------------------------------------------------------------------*/
+ 
+/* ------------------------------------------------------------------------------------------*/
+ 
+ 
+int main()
+{
+    //std::ios::sync_with_stdio(false);
+    //cin.tie(0);
+    //cout.tie(0);
+    //freopen("input.in","r",stdin);
+    //freopen("output.out","w",stdout);
+/* --------------------------------------------------------------------------------------*/
+    cin >> N >> M >> T;
+    if((M - N) >=0 && (T - N)>=0)
+    {
+        cout << "Yes" <<endl;
+    }
+    else cout << "No" << endl;
+}
