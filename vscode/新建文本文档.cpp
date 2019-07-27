69003961c09bb52524ad0a3b8c598846e654344b
HXYerror <zx841985525@163.com>
2019-07-27 23:08:22 +0800

    7.27

---
+++ vscode/新建文本文档.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <utility>
+#define LL long long
+using namespace std;
+const int MAX = 300010;//100000
+const int MAX_1 = 100010;
+const int INF = 0x3f3f3f3f;//1061109567,1e9,int-MAX2147483647;
+const double EPS = 0.0000001;
+const int MOD = 998244353;//998244353;
+const double PI = acos(-1);
+LL T,N,M;
+/*-------------------------------------------------------------------------------------------*/
+ 
+/* ------------------------------------------------------------------------------------------*/
+ 
+ 
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    //freopen("input.in","r",stdin);
+    //freopen("output.out","w",stdout);
+/* --------------------------------------------------------------------------------------*/
+    cin >> T;
+    LL a,b,c;
+    while(T--)
+    {
+        cin >> a >> b >> c;
+        cout << (a + b + c) / 2 <<endl;
+    }
+    return 0;
+}
